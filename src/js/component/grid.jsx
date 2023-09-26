6ddff2309cf519ec1e464e7a7720d61bf8f6dc1b
Christa Kelly <46685557+christadkelly@users.noreply.github.com>
2023-09-26 04:36:12 +0000

    finished basic game, with winner identified

--- src/js/component/grid.jsx
+++ src/js/component/grid.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Grid = (prop) => {
     return (
-        <div className="container">
+        <div className="container-fluid">
 		    <div className="row">
 		        <div className="col" onClick={prop.clickHandler}></div>
 				<div className="col" onClick={prop.clickHandler}></div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import Title from "./title"
 import PlayerMessage from "./playerMessage";
-import Grid from "./grid";
 
-//create your first component
 const Home = () => {
 	const [turn, setTurn] = useState ('X');
-	const [message, setMessage] = useState("It is X's turn!")
+	const [message, setMessage] = useState("It is X's turn!");
+	const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
 
 	const turnChanger = () => {
 		if (turn === 'X') {
@@ -16,22 +15,62 @@ const Home = () => {
 			setTurn('X');
 			setMessage("It is X's turn!");
 		}
-	}
+	};
 
 	const clickHandler = (e) => {
 		if(e.target.innerHTML === ''){
 			turn === 'X' ? e.target.innerHTML = 'X' : e.target.innerHTML = 'O';
-			turnChanger(e);
+			turnChanger(e);	
 		}
+		let loc = e.target.id;
+		let currentBoard = board;
+		currentBoard.splice(loc, 1, turn);
+		setBoard(currentBoard);
+		checkWin(board);
+	};
+
+	const checkWin = (board) => {
+		const winPatterns = [
+			[0, 1, 2], [3, 4, 5], [6, 7, 8],
+			[0, 3, 6], [1, 4, 7], [2, 5, 8],
+			[0, 4, 8], [2, 4, 6]
+		];
+
+		for(let pattern of winPatterns) {
+			let [a, b, c] = pattern;
+			if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+				console.log(board[a]);
+				setMessage(board[a]+" wins!") 
+			}
+		}
+		return null;
 	}
+
+	const reset = () => {
+		setTurn('X');
+		setBoard(['', '', '', '', '', '', '', '', '']);
+		setMessage("It is X's turn!");
+	};
 
 	return (
 		<div>
 			<Title />
 			<PlayerMessage 
 				message = {message}/>
-			<Grid 
-				clickHandler= {clickHandler}/>
+			<div className="container-fluid">
+				{board.map((value, i) => (
+					<div 
+						className="tile" 
+						onClick={clickHandler}
+						key={i}
+						id={i}>
+							{value}
+					</div>
+				))}
+			</div>
+			<div>
+				<button onClick={reset}>Reset Game!</button>
+			</div>
 		</div>
 		
 	);
